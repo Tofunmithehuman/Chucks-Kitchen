6af981f378ba3503da1d88a0db875e3dce4a77d2
Tofunmithehuman <tofunmithehuman@gmail.com>
2026-02-13 05:31:35 +0100

    feat: add Welcome page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
+import Welcome from "./Pages/Onboarding/Welcome";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Routes>
+      <Route path="/welcome" element={<Welcome />} />
+    </Routes>
+  </div>;
 }
 
 export default App;
