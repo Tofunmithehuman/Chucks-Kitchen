2a5c09f57202b2578e528e5f0eea3057686935fb
Tofunmithehuman <tofunmithehuman@gmail.com>
2026-02-13 05:54:17 +0100

    feat: add ScrollToTop component globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
+import ScrollToTop from "./Components/ScrollToTop";
 import Welcome from "./Pages/Onboarding/Welcome";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/welcome" element={<Welcome />} />
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/welcome" element={<><ScrollToTop /><Welcome /></>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
