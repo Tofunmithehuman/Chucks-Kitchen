ae0adcd2265e0cd857961f591851dbb82dad6e89
Tofunmithehuman <tofunmithehuman@gmail.com>
2026-02-14 16:15:35 +0100

    feat: added signin route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import ScrollToTop from "./Components/ScrollToTop";
 import Welcome from "./Pages/Onboarding/Welcome";
-
+import Signin from "./Pages/Signin";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/welcome" element={<><ScrollToTop /><Welcome /></>} />
+        <Route path="/signin" element={<><ScrollToTop /><Signin /></>} />
       </Routes>
     </div>
   );
