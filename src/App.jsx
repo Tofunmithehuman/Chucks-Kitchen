21e4ed64637f1086d5fdd2ad8ac6d53e59c07930
Tofunmithehuman <tofunmithehuman@gmail.com>
2026-02-16 17:39:55 +0100

    feat:added sighup route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router";
 import ScrollToTop from "./Components/ScrollToTop";
 import Welcome from "./Pages/Onboarding/Welcome";
 import Signin from "./Pages/Signin";
+import SignUp from "./Pages/Signup";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/welcome" element={<><ScrollToTop /><Welcome /></>} />
         <Route path="/signin" element={<><ScrollToTop /><Signin /></>} />
+        <Route path="/signup" element={<><ScrollToTop /><SignUp /></>} />
       </Routes>
     </div>
   );
