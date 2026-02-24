4eb35af3ebb668dc0f71f4c6dcf0a79e3cdfc6b4
Tofunmithehuman <tofunmithehuman@gmail.com>
2026-02-24 23:12:15 +0100

    fix: update font size for better responsiveness

--- src/Pages/Onboarding/Welcome.jsx
+++ src/Pages/Onboarding/Welcome.jsx
@@ -78,7 +78,7 @@ const Welcome = () => {
 
           <div className="w-full lg:w-1/2">
             <motion.div
-              className="w-full max-w-3xl mx-auto sm:py-6 px-6 sm:px-10 flex flex-col justify-center min-h-screen"
+              className="w-full max-w-xl mx-auto sm:py-6 px-6 sm:px-10 flex flex-col justify-center min-h-screen"
               initial="hidden"
               animate="visible"
               variants={containerVariants}
@@ -87,7 +87,7 @@ const Welcome = () => {
                 className="flex items-center justify-between mb-auto py-4"
                 variants={itemVariants}
               >
-                <h1 className="island-moments text-[#FF7A18] text-4xl">
+                <h1 className="island-moments text-[#FF7A18] text-3xl">
                   Chuks Kitchen
                 </h1>
                 <motion.div
@@ -96,7 +96,7 @@ const Welcome = () => {
                 >
                   <Link
                     to="/signin"
-                    className="text-[#1E88E5] border-2 border-[#1E88E5] rounded-lg w-30 h-12 flex items-center justify-center text-center transition-colors hover:bg-[#1E88E5] hover:text-white duration-300"
+                    className="text-[#1E88E5] border-2 border-[#1E88E5] rounded-lg w-26 h-10 text-sm flex items-center justify-center text-center transition-colors hover:bg-[#1E88E5] hover:text-white duration-300"
                   >
                     Sign In
                   </Link>
@@ -105,10 +105,10 @@ const Welcome = () => {
 
               <div className="py-8">
                 <motion.div className="space-y-2" variants={itemVariants}>
-                  <h1 className="font-bold text-3xl leading-10.5">
+                  <h1 className="font-bold text-2xl leading-10.5">
                     Your Authentic Taste of Nigeria
                   </h1>
-                  <p className="text-sm leading-6">
+                  <p className="text-xs leading-5">
                     Experience homemade flavors delivered fresh to your desk or
                     home. We bring the rich culinary heritage of Nigeria right
                     to your doorstep.
@@ -122,13 +122,17 @@ const Welcome = () => {
                   {features.map((feature, index) => (
                     <motion.div
                       key={index}
-                      className={`flex items-center gap-2 font-semibold bg-[#F3F4F6] sm:bg-transparent p-3 sm:p-0 rounded-lg ${feature.colSpan}`}
+                      className={`flex items-center gap-2 font-semibold bg-[#F3F4F6] sm:bg-transparent p-3 sm:p-0 rounded-lg `}
                       variants={featureVariants}
                       whileHover={{ scale: 1.03 }}
                       transition={{ duration: 0.2 }}
                     >
-                      <img src={feature.icon} alt={feature.text} />
-                      <p className="text-sm">{feature.text}</p>
+                      <img
+                        src={feature.icon}
+                        className="w-6"
+                        alt={feature.text}
+                      />
+                      <p className="text-xs">{feature.text}</p>
                     </motion.div>
                   ))}
                 </motion.div>
@@ -142,7 +146,7 @@ const Welcome = () => {
                     whileTap={{ scale: 0.98 }}
                     transition={{ duration: 0.2 }}
                   >
-                    <Link className="text-sm bg-[#FF7A18] w-full rounded-lg h-15 flex items-center justify-center text-center text-white font-semibold hover:bg-[#FF8C3A] transition-all duration-300">
+                    <Link className="text-xs bg-[#FF7A18] w-full rounded-lg h-12 flex items-center justify-center text-center text-white font-semibold hover:bg-[#FF8C3A] transition-all duration-300">
                       Start Your Order
                     </Link>
                   </motion.div>
@@ -151,7 +155,7 @@ const Welcome = () => {
                     whileTap={{ scale: 0.98 }}
                     transition={{ duration: 0.2 }}
                   >
-                    <Link className="text-sm border-2 border-[#1E88E5] w-full rounded-lg h-15 flex items-center justify-center text-center text-[#1E88E5] font-semibold hover:bg-[#1E88E5] hover:text-white transition-all duration-300">
+                    <Link className="text-xs border-2 border-[#1E88E5] w-full rounded-lg h-12 flex items-center justify-center text-center text-[#1E88E5] font-semibold hover:bg-[#1E88E5] hover:text-white transition-all duration-300">
                       Learn More About Us
                     </Link>
                   </motion.div>
@@ -163,7 +167,7 @@ const Welcome = () => {
                 variants={itemVariants}
               >
                 <div className="border border-t-2 border-[#F3F4F6]"></div>
-                <div className="flex items-center justify-center gap-4 mt-4 text-xs sm:text-sm py-4">
+                <div className="flex items-center justify-center gap-4 mt-4 text-xs py-4">
                   <p>© {currentYear} Chuks Kitchen.</p>
                   <motion.div whileHover={{ scale: 1.05 }}>
                     <Link className="text-[#64B5F6] hover:underline transition-all duration-200">
